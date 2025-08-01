f3dd8600145978845cdfab4b98d32bbab6a918cf
Majo-DeLaTorre <mariajosedelatorre@outlook.com>
2025-08-01 11:54:32 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
-  basePath: '',
-  assetPrefix: 'https://majo-delatorre.github.io/', // tu URL de GitHub Pages
-};
-
-module.exports = nextConfig;
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
